21edfe068cc1c7abc80ac5b3a20c1a37d8f88935
Coding Unicorn <co.unicorn.ding@gmail.com>
2019-12-27 15:09:48 +0200

    attempt to split cloud fn into smaller functions internally

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.api = async (req, res) => {
   const api = req.path;
-  if (!api) {
+  if (!api || api == "/") {
     res.send("Nothing valuable at this endpoint.")
   }
   console.log("requesting " + api)
