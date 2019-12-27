c76302a94a1254193e9786c311322dc3dded84fb
Coding Unicorn <co.unicorn.ding@gmail.com>
2019-12-27 15:44:05 +0200

    add cors handling

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+function handleCors(req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  }
+}
+
 exports.api = async (req, res) => {
+  handleCors(req, res);
   const api = req.path;
   if (!api || api == "/") {
     res.send("Nothing valuable at this endpoint.")
