e261d9885a81941be44668c686fc88072c2e4cf2
Coding Unicorn <co.unicorn.ding@gmail.com>
2019-12-28 23:23:36 +0200

    limit data size when fetching events

--- api/web/events-fetch/index.js
+++ api/web/events-fetch/index.js
@@ -1,10 +1,22 @@
 const { Datastore } = require('@google-cloud/datastore');
 
-module.exports = async (_, res) => {
+module.exports = async (req, res) => {
 
   const datastore = new Datastore();
+  
+  const limit = req.query.limit;
 
-  const query = datastore.createQuery("Event");
-  const result = await datastore.runQuery(query);
-  res.json(result);
+  let query = datastore
+    .createQuery("Event")
+    .filter('startDate', '>', new Date())
+    .order('startDate');
+
+  
+  if (limit) {
+    query = query.limit(limit);
+  }
+
+  const [entities, info] = await datastore.runQuery(query);
+
+  res.json([entities, info]);
 }
