8994537cdcd0d10aecfbac97ffa826ba8e5dbff2
Coding Unicorn <co.unicorn.ding@gmail.com>
2019-12-29 17:21:32 +0200

    add optional continent and country to query

--- api/web/events-fetch/index.js
+++ api/web/events-fetch/index.js
@@ -6,20 +6,35 @@ module.exports = async (req, res) => {
   
   const limit = req.query.limit;
   const start = req.query.start;
+  const continent = req.query.continent;
+  const country = req.query.country;
 
-  const totalQuery = datastore
-    .createQuery("Event")
-    .select('__key__')
-    .filter('startDate', '>', new Date());
+  const withOptionalContinent = q => continent ? q.filter('continent', '=', continent) : q;
+  const withOptionalCountry = q => country ? q.filter('countryCode', '=', country) : q;
+
+  const totalQuery = withOptionalCountry(
+    withOptionalContinent(
+      datastore
+        .createQuery("Event")
+        .select('__key__')
+        .filter('startDate', '>', new Date())
+    )
+  );
+
+    
 
   const [keys] = (await datastore.runQuery(totalQuery));
   const total = keys.length;
 
-
-  let fetchQuery = datastore
-    .createQuery("Event")
-    .order('startDate')
-    .filter('startDate', '>', new Date());
+  let fetchQuery = withOptionalCountry(
+      withOptionalContinent(
+        datastore
+          .createQuery("Event")
+          .order('startDate')
+          .filter('continent', continent)
+          .filter('startDate', '>', new Date())
+      )
+  );
 
   if (limit) { fetchQuery = fetchQuery.limit(limit); }
   if (start) { fetchQuery = fetchQuery.start(start); }
@@ -27,4 +42,7 @@ module.exports = async (req, res) => {
   const [entities, info] = await datastore.runQuery(fetchQuery);
 
   res.json([entities, { total: total, info }]);
+
+
 }
+
