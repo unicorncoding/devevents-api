c1f89283db2c169454d73ec10a0d00b6f24449f6
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-01-25 12:26:15 +0200

    fetch by topic

--- routes/events.js
+++ routes/events.js
@@ -6,9 +6,11 @@ const datastore = new Datastore();
 
 router.get('/search', asyncHandler(async(req, res) => {
 
-  const { cfp, start, continent, country, limit = 30 } = req.query;
+  const { cfp, start, continent, country, topic, limit = 30 } = req.query;
 
   const withCountry = q => country && continent ? q.filter('countryCode', '=', continent + "/" + country) : q;
+  const withContinent = q => continent ? q.filter('continentCode', '=', continent) : q;
+  const withTopic = q => topic ? q.filter('topic', '=', topic) : q;
   const withStart = q => start ? q.start(start) : q;
 
   const orderProperty = cfp ? 'cfpEndDate' : 'startDate';
@@ -18,6 +20,8 @@ router.get('/search', asyncHandler(async(req, res) => {
         .select('__key__')
         .filter(orderProperty, '>=', new Date());
   totalQuery = withCountry(totalQuery);
+  totalQuery = withContinent(totalQuery);
+  totalQuery = withTopic(totalQuery);
  
   const [keys] = await datastore.runQuery(totalQuery);
   const total = keys.length;
@@ -29,6 +33,8 @@ router.get('/search', asyncHandler(async(req, res) => {
           .order(orderProperty);
   fetchQuery = withCountry(fetchQuery);
   fetchQuery = withStart(fetchQuery);
+  fetchQuery = withContinent(fetchQuery);
+  fetchQuery = withTopic(fetchQuery);
 
   const [entities, info] = await datastore.runQuery(fetchQuery);
 
