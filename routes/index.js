a8c2538d637b7428a29334118be09db3439803bb
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-05-26 12:53:33 +0700

    disable /importers /rss /cal endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 module.exports = (app) => {
-  app.use("/api/importers", require("./importers"));
+  // app.use("/api/importers", require("./importers"));
   app.use("/api/sitemap", require("./sitemap"));
   app.use("/api/events/search", require("./search"));
   app.use("/api/events/new", require("./add"));
-  app.use("/api/events/rss", require("./rss"));
-  app.use("/api/events/cal", require("./ical"));
+  // app.use("/api/events/rss", require("./rss"));
+  // app.use("/api/events/cal", require("./ical"));
   app.use("/api/admin", require("./admin"));
   app.use("/api/karma", require("./karma"));
 };
