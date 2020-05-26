bd872d64b0dbee20b80d0ffafe79d529ab1ccd13
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-05-26 13:05:44 +0700

    rip out confs.tech ingest code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 module.exports = (app) => {
-  // app.use("/api/importers", require("./importers"));
   app.use("/api/sitemap", require("./sitemap"));
   app.use("/api/events/search", require("./search"));
   app.use("/api/events/new", require("./add"));
-  // app.use("/api/events/rss", require("./rss"));
-  // app.use("/api/events/cal", require("./ical"));
   app.use("/api/admin", require("./admin"));
   app.use("/api/karma", require("./karma"));
 };
