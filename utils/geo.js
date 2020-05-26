4ec55c723a64af28ee0e45145df0d57f8fbc9cbf
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-05-26 13:10:08 +0700

    remove unnecessary geo code, required by confs.tech

--- utils/geo.js
+++ utils/geo.js
@@ -22,53 +22,11 @@ function normalizedContinent(continent) {
   return continent.replace("NA", "AM").replace("SA", "AM");
 }
 
-function normalizedCountry(country) {
-  return country
-    .replace("U.S.A.", "United States")
-    .replace("U.K.", "United Kingdom")
-    .replace("Deutschland", "Germany")
-    .replace("switzerland", "Switzerland")
-    .replace("online", "Online")
-    .replace("Netherland", "Netherlands")
-    .replace("Germnay", "Germany")
-    .replace("Netherlandss", "Netherlands")
-    .replace("Perú", "Peru")
-    .replace("Niederlande", "Netherlands")
-    .replace("Czech republic", "Czech Republic")
-    .trim();
-}
-
-function continentOf(country) {
-  const match = Object.values(countries).find((it) => it.name == country);
-  if (!match || !match.continent) {
-    throw "Cannot find continent for country " + country;
-  }
-  return match.continent;
-}
-
-function codeOf(country) {
-  const match = Object.entries(countries).find(([, it]) => it.name == country);
-  if (!match) {
-    throw "Cannot find country code for country " + country;
-  }
-  const [countryCode] = match;
-  return countryCode;
-}
-
 function nameBy(countryCode) {
   return countries[countryCode].name;
 }
 
-function emojiBy(code) {
-  const [, countryCode] = code.split("/");
-  return countries[countryCode] ? countries[countryCode].emoji : undefined;
-}
-
 module.exports.nameBy = nameBy;
-module.exports.emojiBy = emojiBy;
-module.exports.normalizedCountry = normalizedCountry;
-module.exports.continentOf = continentOf;
-module.exports.codeOf = codeOf;
 module.exports.continents = continents;
 module.exports.countries = countries;
 module.exports.countriesOrdered = Object.keys(countries)
