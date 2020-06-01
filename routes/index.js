3a9435a01b0313e9c43fbdf44aff313dc56d6ff4
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-06-01 19:40:01 +0700

    fix conversion

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
-  app.use("/api/sitemap", require("./sitemap"));
   app.use("/api/events/search", require("./search"));
-  app.use("/api/events/new", require("./add"));
   app.use("/api/admin", require("./admin"));
   app.use("/api/karma", require("./karma"));
+  app.use("/api/sitemap", require("./sitemap"));
+  app.use("/api/events/new", require("./add"));
 };
--- utils/pricing.js
+++ utils/pricing.js
@@ -1,5 +1,3 @@
-const is = require("is_js");
-
 const USD = require("./usd");
 const EUR = require("./eur");
 const exchanges = { USD, EUR };
@@ -10,18 +8,26 @@ module.exports.localPrice = (event, targetCurrency) => {
     return undefined;
   }
 
+  const exchange = exchanges[targetCurrency];
+  if (!exchange) {
+      console.warn(`No exchange for target currency: ${targetCurrency}`);
+      return undefined;
+  }
+
+  const convert = exchange[priceCurrency];
+  if (!convert) {
+    console.warn(`No exchange for ${priceCurrency} to ${targetCurrency}`);
+    return undefined;
+  }
+
   return {
-    from: this.convert(priceFrom, priceCurrency, targetCurrency),
-    to: this.convert(priceTo, priceCurrency, targetCurrency),
+    from: this.convert(priceFrom, priceCurrency, targetCurrency, convert),
+    to: this.convert(priceTo, priceCurrency, targetCurrency, convert),
     curr: targetCurrency,
   };
 };
 
-module.exports.convert = (amount, sourceCurrency, targetCurrency) => {
-  if (sourceCurrency === targetCurrency) {
-    return Math.round(amount);
-  }
-
-  const { rate } = exchanges[targetCurrency][sourceCurrency];
+module.exports.convert = (amount, sourceCurrency, targetCurrency, exchange) => {
+  const { rate } = exchange;
   return Math.round(amount * rate);
 };
