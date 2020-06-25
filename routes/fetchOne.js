608039a46e13b5cfc6796cbd18527d7a58878eee
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-06-25 15:56:15 +0700

    is preview available?

--- routes/fetchOne.js
+++ routes/fetchOne.js
@@ -1,16 +1,24 @@
 console.time("initializing fetchOne");
 const asyncHandler = require("express-async-handler");
 const router = require("express").Router();
+const axios = require("axios");
 const { fetchOne } = require("../utils/datastore");
+const normalizeUrl = require("normalize-url");
+
 console.timeEnd("initializing fetchOne");
 
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
     const id = req.params.id;
-
     const conf = await fetchOne(id);
-    res.send(conf);
+    const httpsUrl = normalizeUrl(conf.url, { forceHttps: true });
+    const previewAvailable = await axios
+      .get(httpsUrl)
+      .then((response) => !response.headers["x-frame-options"])
+      .catch(() => false);
+
+    res.send({ ...conf, previewAvailable });
   })
 );
 
